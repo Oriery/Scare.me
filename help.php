fb4556775e58b210b9151f9dde4e935a29e0877c
VanillaFury <83728532+VanillaFury@users.noreply.github.com>
2022-04-11 16:38:20 +0300

    комменты добавил

--- help.php
+++ help.php
@@ -1,13 +1,16 @@
 <?php
 
+// Импорт html
 $main_template = file_get_contents("./html/templ_main.html");
-
 $header = file_get_contents("./html/header.html");
 $content = file_get_contents("./html/helpContent.html");
 $footer = file_get_contents("./html/footer.html");
 
+// Всё, что должно быть добавлено в head темплейта
+// TODO: Скорее всего это плохой вариант
 $add_to_head = '<link rel="stylesheet" href="../style/helpStyle.css">';
 
+// Подсветка выбранной страницы в меню хедера
 $header = preg_replace_callback("{{selected:\d}}", 
     function($m) {
         if (str_contains($m[0], "3")) {     
@@ -18,6 +21,7 @@ $header = preg_replace_callback("{{selected:\d}}",
     }, 
     $header);
 
+// просто всё подставляем в темплейт
 $main_template= str_replace('{add_to_head}', $add_to_head, $main_template);
 $main_template= str_replace('{header}', $header, $main_template);
 $main_template= str_replace('{content}', $content, $main_template);
--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
 
+// Для дебага удобно
 function console_log( $data ){
     echo '<script>';
     echo 'console.log('. json_encode( $data ) .')';
     echo '</script>';
 }
 
+// Импорт html
 $main_template = file_get_contents("./html/templ_main.html");
-
 $header = file_get_contents("./html/header.html");
 $content = file_get_contents("./html/lending.html");
 $footer = file_get_contents("./html/footer.html");
 
+// Всё, что должно быть добавлено в head темплейта
+// TODO: Скорее всего это плохой вариант
 $add_to_head = '<link rel="stylesheet" type="text/css" href="../style/lendingStyle.css" />';
 
+// Подсветка выбранной страницы в меню хедера
 $header = preg_replace_callback("{{selected:\d}}", 
     function($m) {
         if (str_contains($m[0], "1")) {     
@@ -24,6 +28,7 @@ $header = preg_replace_callback("{{selected:\d}}",
     }, 
     $header);
 
+// просто всё подставляем в темплейт
 $main_template= str_replace('{add_to_head}', $add_to_head, $main_template);
 $main_template= str_replace('{header}', $header, $main_template);
 $main_template= str_replace('{content}', $content, $main_template);
