0f52a673e62dce81a3f073dfc41ede19faab8663
VanillaFury <83728532+VanillaFury@users.noreply.github.com>
2022-04-11 15:57:39 +0300

    Основа шаблонизатора

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
-include("./html/header.html");
-include("./html/lending.html");
-include("./html/footer.html");
+
+$main_template = file_get_contents("./html/templ_main.html");
+
+$header = file_get_contents("./html/header.html");
+$content = file_get_contents("./html/lending.html");
+$footer = file_get_contents("./html/footer.html");
+
+$add_to_head = '<link rel="stylesheet" type="text/css" href="../style/lendingStyle.css" />';
+
+
+$main_template= str_replace('{add_to_head}', $add_to_head, $main_template);
+$main_template= str_replace('{header}', $header, $main_template);
+$main_template= str_replace('{content}', $content, $main_template);
+$main_template= str_replace('{footer}', $footer, $main_template);
+
+echo $main_template;
--- mercenaries.php
+++ mercenaries.php
@@ -1,4 +1,17 @@
 <?php
-include("./html/header.html");
-include("./html/mercenaryContent.html");
-include("./html/footer.html");
+
+$main_template = file_get_contents("./html/templ_main.html");
+
+$header = file_get_contents("./html/header.html");
+$content = file_get_contents("./html/mercenaryContent.html");
+$footer = file_get_contents("./html/footer.html");
+
+$add_to_head = '<link rel="stylesheet" href="../style/mercenaryStyle.css">';
+
+
+$main_template= str_replace('{add_to_head}', $add_to_head, $main_template);
+$main_template= str_replace('{header}', $header, $main_template);
+$main_template= str_replace('{content}', $content, $main_template);
+$main_template= str_replace('{footer}', $footer, $main_template);
+
+echo $main_template;
