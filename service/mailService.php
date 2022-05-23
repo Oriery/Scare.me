a175e7a497c238164732aa618090951eab9cbc5b
evensky1 <fromthedeepsize@gmail.com>
2022-05-23 20:38:09 +0300

    mail service

---
+++ service/mailService.php
@@ -0,0 +1,36 @@
+<?php
+
+function sendEmail(string $destAddr, string $subject, string $data) {
+    require '../mailer/PHPMailer.php';
+    require '../mailer/SMTP.php';
+    require '../mailer/Exception.php';
+
+    $from = "fromthedeepsize@gmail.com";
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    $mail->IsHTML(true);
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465;
+    $mail->Username = $from;
+    $mail->Password = "dwxlihubhbcuhqlv";
+    $mail->setFrom($from, "ScareMe");
+    $message = htmlspecialchars($data);
+    $message = trim($message);
+
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+
+    $mail->addAddress($destAddr);
+
+    try {
+        if ($mail->send()) {
+            echo "good!";
+        } else {
+            echo "bad!";
+        }
+    } catch (\PHPMailer\PHPMailer\Exception $e) {
+
+    }
+}
